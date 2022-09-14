b474be79aa2f5273f1f80b297f2410526394b941
MatiasArzamendia <arzamendia.matias99@gmail.com>
2022-09-14 19:16:39 -0300

    instancia de class user y metodos

---
+++ index.js
@@ -0,0 +1,49 @@
+class User {
+    constructor(name, surname, books = [], pets = []) {
+        this.name = name;
+        this.surname = surname;
+        this.books = books;
+        this.pets = pets;
+    }
+
+    getFullName() {
+        return `${this.name} ${this.surname}`;
+    }
+
+    addPet(namePet) {
+        this.pets.push(namePet);
+    }
+
+    countPets() {
+        return this.pets.length;
+    }
+
+    addBook(nameBook, nameAuthor) {
+        const infoBook = {
+            name: nameBook,
+            author: nameAuthor,
+        };
+        this.books.push(infoBook);
+    }
+
+    getBookNames() {
+        let names = this.books.map((bookName) => bookName.name);
+        return names;
+    }
+
+    getFullAtributte() {
+        return this;
+    }
+}
+
+const matias = new User("Matias", "Arzamendia");
+
+console.log(matias.getFullName());
+matias.addPet("Poclin");
+matias.addPet("Arena");
+console.log(matias.countPets());
+matias.addBook("El amor en los tiempos del cólera", "Gabriel García Márquez");
+matias.addBook("Harry Potter y la cámara de los secretos", "J. K. Rowling");
+console.log(matias.getBookNames());
+
+// console.log(matias.getFullAtributte());
