787df2adcfb1f717adb4b93f726d09a9263a7a75
MatiasArzamendia <arzamendia.matias99@gmail.com>
2022-09-28 20:36:12 -0300

    servidor con express

--- index.js
+++ index.js
@@ -9,18 +9,35 @@ const getProductsFromFile = (fs) => {
     return arrayFile;
 };
 
-const getRandomProduct = (callback) => {
-    const randomNumber = Math.ceil(Math.random() * callback.length);
-    return callback[randomNumber];
+const getRandomProduct = (fs) => {
+    const randomNumber = Math.floor(
+        Math.random() * getProductsFromFile(fs).length
+    );
+    return getProductsFromFile(fs)[randomNumber];
 };
 
-const random = Math.ceil(Math.random() * getProductsFromFile(fs).length);
+const getProductById = (fs, id) => {
+    const productSelected = getProductsFromFile(fs).filter(
+        (product) => product.id === id
+    );
+    return productSelected;
+};
 
 app.get('/productos', (request, response) =>
     response.send(getProductsFromFile(fs))
 );
 app.get('/productoRandom', (request, response) =>
-    response.send(getRandomProduct(getProductsFromFile(fs)))
+    response.send(getRandomProduct(fs))
+);
+
+getProductsFromFile(fs).forEach((product) => {
+    app.get(`/producto/${product.id}`, (request, response) =>
+        response.send(getProductById(fs, product.id))
+    );
+});
+
+app.get('*', (request, response) =>
+    response.send('<h1>404</h1><h2>Not Found</h2>')
 );
 
 const server = app.listen(PORT, () => console.log('El servidor fue deplegado'));
