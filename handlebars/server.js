98afeb384d596b213584fea894cbcc385767bc7e
MatiasArzamendia <arzamendia.matias99@gmail.com>
2022-10-26 17:58:32 -0300

    formulario y vista de productos con plantilla handlebars

---
+++ handlebars/server.js
@@ -0,0 +1,34 @@
+const { express, Products } = require('../products.js');
+const { create } = require('express-handlebars');
+const app = express();
+const PORT = process.env.port || 8080;
+
+const productContainer = new Products('Products');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const hbs = create({});
+
+app.engine('handlebars', hbs.engine);
+
+app.set('view engine', 'handlebars');
+
+app.set('views', './views');
+
+app.get('/', (request, response) => {
+	response.render('view/form');
+});
+
+app.get('/productos', (request, response) => {
+	response.render('view/product', {
+		products: productContainer.getAllProducts(),
+	});
+});
+app.post('/', (request, response) => {
+	const product = request.body;
+	productContainer.createProduct(product);
+	response.redirect('/');
+});
+
+app.listen(PORT, () => console.log('servidor desplegado'));
