8bb866d74c50c03e0f14d430df6e37e74da47441
MatiasArzamendia <arzamendia.matias99@gmail.com>
2022-10-27 12:06:58 -0300

    estilos en la plantilla de ejs

--- ejs/server.js
+++ ejs/server.js
@@ -1,12 +1,12 @@
 const { express, Products } = require('../products.js');
 const app = express();
-
 const PORT = process.env.port || 8080;
 
 const productContainer = new Products('Products');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
