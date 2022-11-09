dbae3776c1f5abe74710d013391ac4427ec3a191
MatiasArzamendia <arzamendia.matias99@gmail.com>
2022-11-09 17:39:17 -0300

    proyecto web socket, carga de productos

---
+++ websockets/public/index.js
@@ -0,0 +1,45 @@
+const socket = io();
+const formProduct = document.getElementById('form');
+const template = document.getElementById('templateListProduct').content;
+const fragment = document.createDocumentFragment();
+const listContainer = document.querySelector('.listContainer');
+const title = document.getElementById('title');
+
+const getListProduct = async (url) => {
+	const response = await fetch(url);
+	const data = await response.json();
+	return data;
+};
+
+formProduct.addEventListener('submit', (e) => {
+	e.preventDefault();
+	let array = [];
+	formProduct.querySelectorAll('div input').forEach((item) => {
+		array.push([item.name, item.value]);
+		item.value = '';
+		if (item.name === 'title') item.focus();
+	});
+	console.log(array);
+
+	socket.emit('updateView', array);
+});
+
+socket.on('sendProducts', (message) => {
+	if (!message.products.length) {
+		listContainer.innerHTML =
+			'<h1 style="color: #fff;">No se encontraron products</h1>';
+		return;
+	}
+	listContainer.innerText = '';
+	message.products.map((product) => {
+		console.log(product);
+		const clone = template.cloneNode(true);
+		clone.querySelector('.listItem h2').innerText = product.title;
+		clone.querySelector('.listItem p').innerText = product.price;
+		clone.querySelector('.listItem picture img').src = product.thumbnail;
+		clone.querySelector('.listItem picture img').alt = product.title;
+
+		fragment.appendChild(clone);
+	});
+	listContainer.appendChild(fragment);
+});
---
+++ websockets/server.js
@@ -0,0 +1,54 @@
+const { express, Products } = require('../products.js');
+const { create } = require('express-handlebars');
+const { Server: HttpServer } = require('http');
+const { Server: IOServer } = require('socket.io');
+const PORT = process.env.port || 8080;
+
+const app = express();
+const httpServer = new HttpServer(app);
+const socketIO = new IOServer(httpServer);
+
+const productContainer = new Products('Products');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+const hbs = create({});
+
+app.engine('handlebars', hbs.engine);
+
+app.set('view engine', 'handlebars');
+
+app.set('views', './views');
+
+app.get('/', (request, response) => {
+	response.render('view/home');
+});
+app.get('/productos', (req, res) =>
+	res.send(productContainer.getAllProducts())
+);
+app.post('/productos', (request, response) => {
+	const product = request.body;
+	productContainer.createProduct(product);
+	response.redirect('/');
+});
+
+// socket
+
+socketIO.on('connection', (socket) => {
+	socketIO.sockets.emit('sendProducts', {
+		userID: socket.id,
+		products: productContainer.getAllProducts(),
+	});
+	socket.on('updateView', (message) => {
+		const product = Object.fromEntries(message);
+		productContainer.createProduct(product);
+		socketIO.sockets.emit('sendProducts', {
+			userID: socket.id,
+			products: productContainer.getAllProducts(),
+		});
+	});
+});
+
+httpServer.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
